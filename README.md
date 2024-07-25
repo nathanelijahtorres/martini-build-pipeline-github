1ad0ed5355ef2251a3f94591389fc790089da85d
Rafal Janicki <rafal@kult.io>
2024-07-25 16:15:26 +0200

    Martini upload package action - v1

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# martini-upload-package-action
+# Martini upload package action
+
+This action zips and uploads a Martini package to a Martini instance. For an example package and usage please refer to
+the [sample repository](https://github.com/torocloud/sample-martini-repository)
+
+## Usage
+
+Here's an example of how to use this action in a workflow file:
+
+```yaml
+name: Example Workflow
+
+on: [push]
+
+jobs:
+  upload_package:
+    name: Upload package
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v4
+      - name: Upload the package
+        uses: actions/martini-upload-package-action@main
+        with:
+          base_url: "http://localhost:8080"
+          user_name: "myuser"
+          user_password: "mycomplexpassword"
+          package_dir: "packages/sample-package"
+```
+
+## Inputs
+
+| Input       | Default | Description                                                                          |
+|-------------|---------|--------------------------------------------------------------------------------------|
+| `base_url`  | N/A     | Base URL of the Martini instance                                                     |
+| `user_name` | N/A     | Name of a user on the Martini instance that should be used for uploading the package |
+| `base_url`  | N/A     | The user's password                                                                  |
+| `base_url`  | N/A     | Path to a directory that contains the package's files                                |
+
+## Outputs
+
+| Output    | Description                     |
+|-----------|---------------------------------|
+| `id`      | ID of the uploaded package      |
+| `name`    | Name of the uploaded package    |
+| `status`  | Status of the uploaded package  |
+| `version` | Version of the uploaded package |
---
+++ index.js
@@ -0,0 +1,90 @@
+const archiver = require('archiver');
+const core = require('@actions/core');
+const fs = require('fs');
+const path = require('path');
+const { URLSearchParams } = require('url');
+
+const MARTINI_BASE_URL = core.getInput('base_url', {
+    required: true,
+});
+
+const MARTINI_USER_NAME = core.getInput('user_name', {
+    required: true,
+});
+
+const MARTINI_USER_PASSWORD = core.getInput('user_password', {
+    required: true,
+});
+
+const PACKAGE_DIR = core.getInput('package_dir', {
+    required: true,
+});
+
+const PACKAGE_NAME = path.basename(PACKAGE_DIR);
+const ZIP_PATH = __dirname + `/${PACKAGE_NAME}.zip`;
+
+async function uploadPackage() {
+    const tokenParams = new URLSearchParams();
+    tokenParams.append('client_id', 'TOROMartini');
+    tokenParams.append('client_secret', 'TOROMartini');
+    tokenParams.append('grant_type', 'password');
+    tokenParams.append('password', MARTINI_USER_PASSWORD);
+    tokenParams.append('username', MARTINI_USER_NAME);
+
+    const tokenResponse = await fetch(`${MARTINI_BASE_URL}/oauth/token`, {
+        body: tokenParams,
+        method: 'POST',
+    });
+    const tokenResponseJson = await tokenResponse.json();
+    if (!tokenResponse.ok) {
+        throw Error(JSON.stringify(tokenResponseJson));
+    }
+
+    const packageData = new FormData();
+    packageData.set('file', await fs.openAsBlob(ZIP_PATH), `${PACKAGE_NAME}.zip`);
+
+    const uploadResponse = await fetch(`${MARTINI_BASE_URL}/esbapi/packages/upload?stateOnCreate=STARTED&replaceExisting=true`, {
+        body: packageData,
+        headers: {
+            Authorization: `Bearer ${tokenResponseJson['access_token']}`,
+        },
+        method: 'POST',
+    });
+    const uploadResponseJson = await uploadResponse.json();
+    if (!uploadResponse.ok || uploadResponseJson.length !== 1) {
+        throw Error(JSON.stringify(uploadResponseJson));
+    }
+
+    return uploadResponseJson[0];
+}
+
+function zipPackage() {
+    const output = fs.createWriteStream(ZIP_PATH);
+    const archive = archiver('zip', {
+        zlib: { level: 9 },
+    });
+
+    return new Promise((resolve, reject) => {
+        output.on('close', () => resolve());
+        archive.on('error', err => reject(err));
+
+        archive.pipe(output);
+        archive.directory(PACKAGE_DIR, PACKAGE_NAME);
+        archive.finalize();
+    });
+}
+
+return zipPackage()
+    .then(uploadPackage)
+    .then(res => {
+        core.info('Package uploaded successfully');
+
+        core.setOutput('id', res.id);
+        core.setOutput('name', res.name);
+        core.setOutput('status', res.status);
+        core.setOutput('version', res.version);
+    })
+    .catch((err) => {
+        core.error(err);
+        core.setFailed(err.message);
+    });
